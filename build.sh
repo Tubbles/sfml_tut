8e08870a9ed6721c670203699370a3248c97a170
Tubbles <jae91m@gmail.com>
2023-01-01 21:29:16 +0100

    Add fmt, imgui, spdlog

---
+++ build.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+set -e
+
+my_dir="$(dirname "$(realpath "$0")")"
+target_dir="$my_dir/target"
+app_name="$(grep -E '^project(.+)' CMakeLists.txt | sed -E 's/project\((.+)\)/\1/g')"
+
+implicit_build=true
+build=false
+clean=false
+run=false
+verbose=false
+extra_verbose=false
+target="debug"
+target_dir_target="$target_dir/debug"
+
+usage="Usage: $0 [options]
+where [options] can be zero or more of:
+  ?,h,help,--help     display this help text and exit
+  v,verbose           run more verbosely
+  extra-verbose       run with all verbose text
+  debug,release       build for debug (default) or release target
+  clean               clean everything, does not build implicitly
+  build               build, implicitly selected
+  run                 run the application after a successful build, forces build"
+
+while (($#)); do
+case $1 in
+\?|h|help|--help)
+    echo "$usage"
+    exit 0
+    ;;
+v|verbose)
+    verbose=true
+    ;;
+extra-verbose)
+    verbose=true
+    extra_verbose=true
+    set -x
+    ;;
+debug)
+    target="debug" || $run == true
+    target_dir_target="$target_dir/debug"
+    ;;
+release)
+    target="release"
+    target_dir_target="$target_dir/release"
+    ;;
+clean)
+    clean=true
+    implicit_build=false
+    ;;
+build)
+    build=true
+    ;;
+run)
+    build=true
+    run=true
+    ;;
+*)
+    echo "Unknown argument: \"$1\""
+    echo "$usage"
+    exit 1
+    ;;
+esac
+shift
+done
+
+cmake_debug=""
+if [[ $target == "debug" ]]; then
+    cmake_debug="-DCMAKE_BUILD_TYPE=Debug"
+elif [[ $target == "release" ]]; then
+    cmake_debug="-DCMAKE_BUILD_TYPE=Release"
+fi
+
+ninja_verbose=""
+if [[ $verbose == true ]]; then
+    ninja_verbose="-v"
+fi
+
+cmake_verbose=""
+if [[ $extra_verbose == true ]]; then
+    cmake_files=$(find "$my_dir" \( -name CMakeLists.txt -or -name '*.cmake' \) -not -path '*/modules/*/*' -not -path '*/target/*' | paste -sd' ')
+    cmake_verbose="--trace-expand "
+    for file in $cmake_files; do
+        cmake_verbose+="--trace-source=$file "
+    done
+fi
+
+if [[ $implicit_build == true ]]; then
+    build=true
+fi
+
+if [[ $clean == true ]]; then
+    rm -fr "$target_dir"
+    rm -fr "$my_dir/compile_commands.json"
+fi
+
+if [[ $build == true ]]; then
+    mkdir -p "$target_dir_target"
+    cmake $cmake_verbose -S "$my_dir" -B "$target_dir_target" -G Ninja $cmake_debug
+    cmake --build "$target_dir_target" -- $ninja_verbose
+    if [[ $run == true ]]; then
+        "$target_dir_target/$app_name"
+    fi
+fi
+
+cp -f "$target_dir_target/compile_commands.json" "$my_dir" 2>/dev/null || :
--- main.cc
+++ main.cc
@@ -1,24 +1,105 @@
-#include <SFML/Graphics.hpp>
+#include "modules/spdlog/include/spdlog/sinks/basic_file_sink.h"
+#include "modules/spdlog/include/spdlog/sinks/stdout_color_sinks.h"
+#include "modules/spdlog/include/spdlog/spdlog.h"
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <filesystem>
+#include <fmt/core.h>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+#ifndef NDEBUG
+#include "modules/imgui-sfml/imgui-SFML.h"
+#include "modules/imgui/imgui.h"
+#endif
+
+namespace fs = std::filesystem;
+
+static void setup_spdlog(fs::path &my_dir) {
+    static const std::string log_filename{my_dir.string() + std::string{"/log.txt"}};
+
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_level(spdlog::level::warn);
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_filename.c_str(), true);
+    file_sink->set_level(spdlog::level::trace);
+    spdlog::sinks_init_list sinks{console_sink, file_sink};
+
+    auto logger = std::make_shared<spdlog::logger>("main", sinks);
+    logger.get()->set_level(spdlog::level::debug);
+    spdlog::set_default_logger(logger);
+}
+
+#ifndef NDEBUG
+static void setup_imgui(fs::path &my_dir, sf::RenderWindow &window) {
+    static const std::string imgui_ini_filename{my_dir.string() + std::string{"/imgui.ini"}};
+    static const std::string imgui_log_filename{my_dir.string() + std::string{"/imgui_log.txt"}};
+
+    if (!ImGui::SFML::Init(window)) {
+        spdlog::error("ImGui initialization failed");
+    }
+
+    ImGui::GetIO().IniFilename = imgui_ini_filename.c_str();
+    ImGui::GetIO().LogFilename = imgui_log_filename.c_str();
+}
+#endif
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args(argv, argv + argc);
+    fs::path prog{args[0]};
+    fs::path my_dir{fs::canonical(prog.parent_path()).string()};
+
+    setup_spdlog(my_dir);
+    spdlog::info("Application started");
+
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Tutorial");
+    window.setVerticalSyncEnabled(true);
+
+#ifndef NDEBUG
+    setup_imgui(my_dir, window);
+    spdlog::info("ImGui started");
+#endif
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+    sf::Clock deltaClock;
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+#ifndef NDEBUG
+            ImGui::SFML::ProcessEvent(window, event);
+#endif
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
                 window.close();
         }
 
+#ifndef NDEBUG
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("FPS");
+        ImGui::Text("%.1f", ImGui::GetIO().Framerate);
+        ImGui::End();
+#endif
+
         window.clear();
         window.draw(shape);
+
+#ifndef NDEBUG
+        ImGui::SFML::Render(window);
+#endif
+
         window.display();
     }
+
+#ifndef NDEBUG
+    ImGui::SFML::Shutdown();
+#endif
 
     return 0;
 }
